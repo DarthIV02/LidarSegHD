a0a87ae3f7704d60e4740d93bb4f20469fcd2e63
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-02-09 13:13:06 -0800

    Update LiDARSeg with first try of encoding

--- models/LiDARSegHD/dataloader.py
+++ models/LiDARSegHD/dataloader.py
@@ -1,13 +1,15 @@
 import numpy as np
+import torch
 
 def read_points(bin_file):
     points = np.fromfile(bin_file, dtype = np.float32)
     points = np.reshape(points,(-1,4)) # x,y,z,intensity
-    return points
+    return torch.from_numpy(points)
 
 def read_semlabels(label_file):
     semlabels = np.fromfile(label_file, dtype = np.uint32) & 0xffff
-    return semlabels
+    semlabels = semlabels.astype(np.uint8)
+    return torch.from_numpy(semlabels)
 
 if __name__ == "__main__":
     points = read_points('Dataset_TLS/dense_dataset_semantic/sequences/00/velodyne/000001.bin')
--- models/LiDARSegHD/main.py
+++ models/LiDARSegHD/main.py
@@ -1,27 +1,199 @@
-import math
-import torch
 import torchhd
-from dataloader import read_points
-from sklearn import preprocessing
+from dataloader import read_points, read_semlabels
+import numpy as np
+import torch
+from tqdm import tqdm
+import os
+from scipy.stats import norm
+from scipy.special import softmax
+from sklearn import neighbors
 
-def create_classes(num_classes):
-    "Class hypervectors which don't have relationships between each other" 
-    classes_hv = torchhd.random(num_classes, d)
-    return classes_hv
+class SegmentHD:
 
-if __name__ == "__main__":
-    d = 500 # dimensions
-    #classes = torchhd.random(5, d) # alive, 1h, 10h, 100h, 1000h
-    i_emb = torchhd.embeddings.Level(10, d, low=0, high=1) # Intensity
-    x_emb = torchhd.embeddings.Level(100, d, low=-15, high=15)
-    y_emb = torchhd.embeddings.Level(100, d, low=-15, high=15) 
-    z_emb = torchhd.embeddings.Level(100, d, low=0, high=2)
-    var = torchhd.random(4, d) # x, y, z, i
+    def __init__(self, d=1000, num_classes=5, num_closest_points=5, training_samples_per_class = 100, lr = 0.01) -> None:
+        self.d = d
+        self.num_classes = num_classes
+        self.num_closest_points = num_closest_points
+        self.lr = lr
+        #self.i_emb = torchhd.embeddings.Level(1000, d, low=0, high=1) # Intensity
+        #self.i_emb = torchhd.embeddings.Projection(self.num_closest_points, d) # Random Projection
+        #self.x_emb = torchhd.embeddings.Level(1000, d, low=-0.01, high=0.01)
+        #self.y_emb = torchhd.embeddings.Level(1000, d, low=-0.01, high=0.01) 
+        #self.z_emb = torchhd.embeddings.Level(1000, d, low=-0.01, high=0.01)
+        self.x_emb = torchhd.embeddings.Projection(self.num_closest_points, d)
+        self.y_emb = torchhd.embeddings.Projection(self.num_closest_points, d) 
+        self.z_emb = torchhd.embeddings.Projection(self.num_closest_points, d)
+        self.var = torchhd.random(3, d) # x, y, z, i 
+        self.classes_hv = torch.zeros(classes, d) # alive, 1h, 10h, 100h, 1000h
+        self.training_samples_per_class = training_samples_per_class
+        self.count_matrix = torch.zeros(self.num_classes, self.num_classes)
+        self.softmax = torch.nn.Softmax(dim=0)
+    
+    def to(self, *args):
+        '''
+        Moves data to the device specified, e.g. cuda, cpu or changes
+        dtype of the data representation, e.g. half or double.
+        Because the internal data is saved as torch.tensor, the parameter
+        can be anything that torch accepts. The change is done in-place.
 
-    points = read_points('Dataset_TLS/dense_dataset_semantic/sequences/00/velodyne/000001.bin')
-    points[:,3] = preprocessing.normalize([points[:,3]]) #Normalize intensity
+        Args:
+            device (str or :class:`torch.torch.device`) Device to move data.
 
-    enc_points = var[0] * x_emb(torch.tensor(points[:, 0])) + var[1] * y_emb(torch.tensor(points[:, 1])) + var[2] * z_emb(torch.tensor(points[:, 2])) + var[3] * i_emb(torch.tensor(points[:, 3]))
+        Returns:
+            :class:`SegmentHD`: self
+        '''
+
+        self.classes_hv = self.classes_hv.to(*args)
+        self.var = self.var.to(*args)
+        self.count_matrix = self.count_matrix.to(*args)
+        self.x_emb = self.x_emb.to(*args)
+        self.y_emb = self.y_emb.to(*args)
+        self.z_emb = self.z_emb.to(*args)
+        return self
+
+    def encode_points(self, x, y, z, int=None):
+        # Normalize xyz
+        #x /= 0.4
+        #y /= 0.4
+        #z /= 0.3
+
+        # Encode points x,y,z,int = Torch vectors
+        enc_points = (self.var[0] * self.x_emb(torch.tensor(x))) + (self.var[1] * self.y_emb(torch.tensor(y))) + (self.var[2] * self.z_emb(torch.tensor(z))) # + (self.var[3] * self.i_emb(torch.tensor(int)))
+        return enc_points.sign()
+    
+    def encode_graph(self, id_point, point, points, distances=None):
+        # close_points = self.get_close_points_sklearn(id_point, A) # <- With sklearn Get indeces for the closest points including itself
+        
+        # Test to encode the vectors based on the closest points
+        if (distances == None):
+            close_points = self.get_close_points_euclidean(point, points)
+            #new_id = np.where(close_points == id_point) # In only closest points
+            #close_points = np.delete(close_points, new_id)
+        else:
+            close_points = distances.indices
+        
+        vectors_closest_points = points[close_points] - point
+
+        enc_vectors = model.encode_points(vectors_closest_points[:, 0], vectors_closest_points[:, 1], vectors_closest_points[:, 2]) # In case of intensity points[close_points][:, 3]
+        #full_point = enc_vectors
+        
+        #edges = torch.zeros(self.num_closest_points, d)
+        #for i in close_points.shape[0]:
+        #    if(i != id_point):
+        #        edges[i] = torchhd.bind(enc_points[i], enc_points[id_point])
+        
+        # If multiple points
+        #full_point = torch.zeros(d) # Create a 0 vector which is going to be added to each one of the encoded points
+        #for e, label in zip(enc_vectors, labels[close_points]): # add edges together
+        #    if labels[id_point] == label:
+        #        full_point = torchhd.bundle(full_point, e)
+        #full_point = torchhd.hard_quantize(full_point) # Fully encoded hv
+        
+        #classes[label-1] = torchhd.bundle(classes[label-1], full_point) # Add to class hv
+        return enc_vectors
+    
+    def similarity(self, point):
+        cos_sim_one = torchhd.cosine_similarity(point, self.classes_hv)
+        cos_sim_one = self.softmax(cos_sim_one)
+        return cos_sim_one
+    
+    def get_close_points_euclidean(self, point, points): # TODO HD KNN? -> HD clustering by whose work has studied 
+        squared_dist = torch.sum((point-points)**2, 1)
+        dist = torch.sqrt(squared_dist)
+        index_closest_points = torch.topk(dist, self.num_closest_points+1, largest=False)
+        return index_closest_points.indices[1:]
+    
+    #def get_close_points_number(self, i, A):
+        
+    #    return A.indices
+    
+    #def get_close_points_radius(self, i, A):
+        
+    #    return A.indices
+
+    def train(self, points, labels):
+        #A = neighbors.kneighbors_graph(points, self.num_closest_points, mode='connectivity', include_self=False) #<- With sklearn
+        #A = neighbors.radius_neighbors_graph(points, 0.5, mode='connectivity', include_self=False)
+        for label in range(1, self.num_classes+1):
+            classes_id = torch.where(labels == label)
+            if(classes_id[0].shape[0] != 0):
+                ids = np.random.choice(range(classes_id[0].shape[0]), size=self.training_samples_per_class, replace=False)
+                training_samples = classes_id[0][ids] # Choose 100 points for training
+                for i, point in zip(training_samples, points[training_samples]): # id, xyz of points
+                    #current_point_graph = model.encode_graph(i, point, points, labels, A[i]) #<- With sklearn
+                    current_point_graph = model.encode_graph(i, point, points)
+                    cos_sim_one = self.similarity(current_point_graph)
+                    max_sim_id = torch.argmax(cos_sim_one).item()
+                    if (max_sim_id != label-1):
+                        self.classes_hv[max_sim_id] -= current_point_graph*self.lr*(1-cos_sim_one[max_sim_id])
+                    self.classes_hv[label-1] += current_point_graph*self.lr*(1-cos_sim_one[label-1])
     
 
-    print(enc_points)
+    def test(self, points, labels):
+        for label in range(1, self.num_classes+1):
+            classes_id = torch.where(labels == label)
+            if(classes_id[0].shape[0] != 0):
+                ids = np.random.choice(range(classes_id[0].shape[0]), size=self.training_samples_per_class, replace=False)
+                training_samples = classes_id[0][ids]
+                for i, point in zip(training_samples, points[training_samples]):
+                    enc_point = self.encode_graph(i, point, points)
+                    sim = self.similarity(enc_point)
+                    pred_class = torch.argmax(sim).item()
+                    self.count_matrix[int(label-1), pred_class] += 1
+        return
+
+if __name__ == '__main__':
+    d = 7500 # dimensions
+    number_points = 500 # Number of closest points to consider for the graph
+    classes = 5
+    number_of_choices = 200
+    epochs = 1
+    lr = 0.01
+    model = SegmentHD(d, classes, number_points, number_of_choices, lr)
+    np.random.seed(0)
+    torch.manual_seed(0)
+
+    print("The hyperparameters are:")
+    print(f"d = {d}")
+    print(f"Number of close points = {number_points}")
+    print(f"Number of points per class / file = {number_of_choices}")
+    print(f"Learning Rate: ", lr)
+
+    if torch.cuda.is_available():
+        print('Training on GPU!')
+        model = model.to('cuda')
+
+    for e in range(epochs):
+        for file in tqdm(os.listdir('Dataset_TLS/dense_dataset_semantic/sequences/00/velodyne/')):
+            file = file[:-4]
+            points = read_points(f'Dataset_TLS/dense_dataset_semantic/sequences/00/velodyne/{file}.bin')[:,:3]
+            labels = read_semlabels(f'Dataset_TLS/dense_dataset_semantic/sequences/00/labels/{file}.label')
+
+            #Normalize intensity
+            #mu, std = norm.fit([points[:,3]])
+            #points[:, 3][points[:, 3] >= std*2] = std*2
+            #points[:,3] /= std*2 
+
+            #Normalize xyz
+            #points[:, :3] /= 0.02
+            points = points.to('cuda')
+            labels = labels.to('cuda')
+            model.train(points, labels)
+
+        print(f"Epoch: {e}")
+        print(model.classes_hv)
+        sim = torchhd.cosine_similarity(model.classes_hv, model.classes_hv)
+        print(sim)
+
+    # Testing
+    for file in tqdm(os.listdir('Dataset_TLS/dense_dataset_semantic/sequences/01/velodyne/')):
+        file = file[:-4]
+        points = read_points(f'Dataset_TLS/dense_dataset_semantic/sequences/01/velodyne/{file}.bin')[:,:3]
+        labels = read_semlabels(f'Dataset_TLS/dense_dataset_semantic/sequences/01/labels/{file}.label')
+
+        points = points.to('cuda')
+        labels = labels.to('cuda')
+
+        model.test(points, labels)
+
+    print(model.count_matrix)
